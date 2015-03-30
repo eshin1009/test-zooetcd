184f9a86043f607fb6dda96fc0e43fef55bda00d
Ubuntu <ubuntu@ip-172-31-40-147.us-west-2.compute.internal>
2015-03-30 18:49:11 +0000

    Simplified read/write

--- etcd/etcd-client.py
+++ etcd/etcd-client.py
@@ -5,24 +5,30 @@ import sys
 maxidx = 100
 leaderIp = '172.31.40.147'
 
-writeratio = client.read('/writeratio')
-readratio = 100-writeratio
+writeratio = client.read('/writeratio') * 10
+readratio = 1000-writeratio
 ip = socket.gethostbyname(socket.gethostname())
 clientId = (ip.split('.'))[-1]
 
 idx = 1
 client = etcd.Client(host=leaderIp, protocol='https') # on port 4001
 while True:
-    for x in range(idx*writeratio):
-        client.write('/nodes/n' + clientId + '_' + str(x), x)
-    max_written_idx = idx*writeratio
-    widx = 0
-    for x in range(idx*readratio):
+    for x in range(writeratio):
+        client.write('/nodes/n' + clientId + '_' + str(x), str(x))
+    for x in range(readratio):
         client.read('/nodes/n' + clientId + '_' + str(widx))
-        widx = widx + 1
-        if widx == max_written_idx:
-            widx = 0
-    idx = idx + 1
-    if idx > maxidx:
-        idx = 1
+
+# while True:
+#     for x in range(idx*writeratio):
+#         client.write('/nodes/n' + clientId + '_' + str(x), str(x))
+#     max_written_idx = idx*writeratio
+#     widx = 0
+#     for x in range(idx*readratio):
+#         client.read('/nodes/n' + clientId + '_' + str(widx))
+#         widx = widx + 1
+#         if widx == max_written_idx:
+#             widx = 0
+#     idx = idx + 1
+#     if idx > maxidx:
+#         idx = 1
 
